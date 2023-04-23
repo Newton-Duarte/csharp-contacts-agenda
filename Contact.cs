b90088cb1aa586674ece364559b4a2e52600b18d
Newton Duarte Cavalcanti <newton_@hotmail.com>
2023-04-23 08:51:19 -0300

    feat: add created at field to contact class

--- Contact.cs
+++ Contact.cs
@@ -4,15 +4,18 @@ class Contact
   public string Name { get; set; }
   public string Number { get; set; }
 
+  public DateTime CreatedAt { get; private set; }
+
   public Contact(int id, string name, string number)
   {
     this.Id = id;
     this.Name = name;
     this.Number = number;
+    this.CreatedAt = DateTime.Now;
   }
 
   public override string ToString()
   {
-    return $"{Id} - {Name} - {Number}";
+    return $"{Id} - {Name} - {Number} - {CreatedAt}";
   }
 }
